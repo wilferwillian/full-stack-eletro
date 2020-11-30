beb774e4cb2013d5dd236a8d5a179a173a6ca474
wilferwillian <wilfer.willian@gmail.com>
2020-11-30 13:48:18 -0300

    criação de menu
    criaçao de projeto de sitew full stack eletro em tecnologias react,php, bootstrap e banco de dados . criação de menu de navegação

---
+++ src/components/menu/index.js
@@ -0,0 +1,10 @@
+import{Navbar,nav} from 'react-bootstrap';
+export default function menu(){
+return(
+        <Navbar className="navbar-light"bg="danger"expand="lg"fixed="top">
+            <Navbar.Brand>
+                Full Stack Eletro
+            </Navbar.Brand>
+        </Navbar>
+    )
+};
