b93ea8dc722895b965013fed8c4f33d19dd4bdb6
wilferwillian <wilfer.willian@gmail.com>
2020-12-02 09:14:20 -0300

    atualizacao de projeto individual
    atualização para corrigir erros de projeto

--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -1,28 +1,28 @@
-import{Navbar,nav, Nav} from 'react-bootstrap';
+import{Navbar,Nav} from 'react-bootstrap';
 import NavbarCollapse from 'react-bootstrap/esm/NavbarCollapse';
-export default function menu(){
+export default function Menu(){
 return(
         <Navbar className="navbar-light"bg="danger"expand="lg"fixed="top">
             <Navbar.Brand>
                 Full Stack Eletro
             </Navbar.Brand>
-            <Navbar.Toggle aria aria-controls="itens menu"/>
-            <Navbar.Collapse id="itens menu">
-                <Nav>
+            <Navbar.Toggle aria aria-controls="item-menu"/>
+            <Navbar.Collapse id="item-menu">
+                <Nav >
                 <Nav.Item>
-                    <Nav.link as={Link} href="/"to="/">Página Inicial</Nav.link>
+                <Nav.link href="/"to="/">Página Inicial</Nav.link>
                 </Nav.Item>
                 <Nav.Item>
-                <Nav.link as={Link} href="/produtos"to="/produtos">Produtos</Nav.link>
+                <Nav.link  href="/produtos"to="/produtos">Produtos</Nav.link>
                 </Nav.Item>
                 <Nav.Item>
-                <Nav.link as={Link} href="/contatos"to="/contatos">Contatos</Nav.link>
+                <Nav.link  href="/contatos"to="/contatos">Contatos</Nav.link>
                 </Nav.Item>
                 <Nav.Item>
-                <Nav.link as={Link} href="/"to="/"></Nav.link>
+                <Nav.link  href="/"to="/"></Nav.link>
                 </Nav.Item>
                 <Nav.Item>
-                <Nav.link as={Link} href="/"to="/"></Nav.link>
+                <Nav.link  href="/"to="/"></Nav.link>
                 </Nav.Item>
                 </Nav>
             </Navbar.Collapse>
