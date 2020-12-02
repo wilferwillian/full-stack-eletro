c74d0bc0d8bb463d0f78c0a913659a4f0df1d858
wilferwillian <wilfer.willian@gmail.com>
2020-12-02 07:03:42 -0300

    Update index.js
    Continuação do projeto individual

--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -1,10 +1,31 @@
-import{Navbar,nav} from 'react-bootstrap';
+import{Navbar,nav, Nav} from 'react-bootstrap';
+import NavbarCollapse from 'react-bootstrap/esm/NavbarCollapse';
 export default function menu(){
 return(
         <Navbar className="navbar-light"bg="danger"expand="lg"fixed="top">
             <Navbar.Brand>
                 Full Stack Eletro
             </Navbar.Brand>
+            <Navbar.Toggle aria aria-controls="itens menu"/>
+            <Navbar.Collapse id="itens menu">
+                <Nav>
+                <Nav.Item>
+                    <Nav.link as={Link} href="/"to="/">Página Inicial</Nav.link>
+                </Nav.Item>
+                <Nav.Item>
+                <Nav.link as={Link} href="/produtos"to="/produtos">Produtos</Nav.link>
+                </Nav.Item>
+                <Nav.Item>
+                <Nav.link as={Link} href="/contatos"to="/contatos">Contatos</Nav.link>
+                </Nav.Item>
+                <Nav.Item>
+                <Nav.link as={Link} href="/"to="/"></Nav.link>
+                </Nav.Item>
+                <Nav.Item>
+                <Nav.link as={Link} href="/"to="/"></Nav.link>
+                </Nav.Item>
+                </Nav>
+            </Navbar.Collapse>
         </Navbar>
     )
 };
